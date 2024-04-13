96534e52f55d884112268cf351326685bd36eb69
mclaren-0110 <adiluz0110@gmail.com>
2024-04-13 16:05:14 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -19,5 +19,12 @@ def predict_api():
     output=reg_model.predict(transformed_data)
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_data=scaler.transform(np.array(data).reshape(1,-1))
+    output=reg_model.predict(final_data)[0]
+    return render_template('home.html',prediction_text="The house price prediction is {}".format(output))
+
 if __name__=="__main__":
     app.run(debug=True)
